45064fb6630aff522106e9b40dbff16d769d1c7b
deetz99 <dietrich@wolpert.ca>
2025-07-23 14:38:22 -0700

    create launchdarkly playground page

---
+++ packages/layers/base/.playground/app/pages/test/launchdarkly.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'connect-base'
+})
+
+const { getStoredFlag, getFeatureFlag, ldClient } = useConnectLaunchDarkly()
+
+const test = getStoredFlag<{ something: 'text' }>('allowable-business-passcode-types')
+
+watchEffect(async () => {
+  // await ldClient.value?.waitUntilReady()
+  console.log('TEST FLAG: ', test.value)
+})
+</script>
+
+<template>
+  <div>
+    LaunchDarkly test page
+    <UButton label="testing" />
+  </div>
+</template>
