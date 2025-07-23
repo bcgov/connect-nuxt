70caf96890db93dcb1c6f86cef7c9de6d9be0d05
deetz99 <dietrich@wolpert.ca>
2025-07-23 14:30:07 -0700

    add system banner component with LD

---
+++ packages/layers/base/app/components/Connect/SystemBanner.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const ld = useConnectLaunchDarkly()
+// const { $sanitize } = useNuxtApp()
+
+defineProps({
+  dismissible: { type: Boolean, default: false },
+  icon: { type: String, default: 'i-mdi-information' }
+})
+
+const close = ref(false)
+const message = ld.getStoredFlag<string>('banner-text')
+</script>
+
+<template>
+  <div class="bg-yellow-400">
+    <UAlert
+      v-show="!!message && !close"
+      class="border-b-2 border-yellow-400 py-0"
+      :description="message"
+      :close="dismissible"
+      close-icon="i-mdi-close"
+      :ui="{
+        root: 'rounded-none bg-yellow-400 p-0 app-inner-container',
+        wrapper: 'bg-yellow-400',
+        close: 'mt-2 text-gray-900'
+      }"
+      @update:open="close = true"
+    >
+      <!-- dismissible ? { class: 'pr-2 text-gray-900' } : null -->
+      <template #description>
+        <div class="flex gap-2 items-center py-2">
+          <UIcon class="size-7 shrink-0 text-gray-900 self-start" :name="icon" />
+          <!-- eslint-disable vue/no-v-html tailwindcss/no-custom-classname -->
+          <p class="vhtml text-gray-900" v-html="message" />
+        </div>
+      </template>
+    </UAlert>
+  </div>
+</template>
+<!-- must style globally for vhtml style to work  -->
+<style>
+.vhtml > a {
+  color: #212529;
+  text-decoration: underline;
+}
+</style>
