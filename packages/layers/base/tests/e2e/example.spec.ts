22a4774426c5dde97b3b097df8f97857bfeb84a5
deetz99 <dietrich@wolpert.ca>
2025-07-24 15:49:28 -0700

    fix e2e setup

--- packages/layers/base/tests/e2e/example.spec.ts
+++ packages/layers/base/tests/e2e/example.spec.ts
@@ -4,6 +4,5 @@ test.describe('Example 1', () => {
   test('Base page loads with expected visuals 1', async ({ page }) => {
     await page.goto('./')
     await expect(page.getByRole('heading', { name: 'Hello World!' })).toBeVisible()
-    await expect(page.getByText('{ "name": "My amazing Base Nuxt layer (playground)" }')).toBeVisible()
   })
 })
