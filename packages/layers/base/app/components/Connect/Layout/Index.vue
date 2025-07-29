6efc3f9f9d53c386e77f6432360e0440dca57021
Kial Jinnah <kialj876@gmail.com>
2025-07-29 16:13:11 -0400

    Initial base adds
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

---
+++ packages/layers/base/app/components/Connect/Layout/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <slot name="header">
+      <ConnectHeader />
+    </slot>
+    <slot name="banner">
+      <ConnectSystemBanner />
+    </slot>
+    <slot name="breadcrumb">
+      <ConnectBreadcrumb />
+    </slot>
+    <div class="app-inner-container app-body">
+      <div class="flex flex-col lg:flex-row lg:gap-6 grow">
+        <div class="grow max-w-full overflow-hidden">
+          <slot />
+        </div>
+      </div>
+    </div>
+    <slot name="footer">
+      <ConnectFooter />
+    </slot>
+  </div>
+</template>
--- packages/layers/base/app/layouts/ConnectBase.vue
+++ packages/layers/base/app/layouts/ConnectBase.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="app-container">
-    <ConnectHeader />
-    <ConnectSystemBanner />
-    <ConnectBreadcrumb />
-    <div class="app-inner-container app-body">
-      <div class="flex flex-col lg:flex-row lg:gap-6 grow">
-        <div class="grow max-w-full overflow-hidden">
-          <slot />
-        </div>
-      </div>
-    </div>
-    <ConnectFooter />
-  </div>
+  <ConnectLayout>
+    <slot />
+  </ConnectLayout>
 </template>
