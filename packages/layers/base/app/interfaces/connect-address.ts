66d897a8ac7b64f49f2575a87f675cb3049ce439
deetz99 <dietrich@wolpert.ca>
2025-07-30 13:32:26 -0700

    add address display

---
+++ packages/layers/base/app/interfaces/connect-address.ts
@@ -0,0 +1,12 @@
+export interface ConnectAddress {
+  street: string
+  streetAdditional: string
+  city: string
+  region: string
+  postalCode: string
+  country: string
+  locationDescription: string
+  streetName?: string
+  streetNumber?: string
+  unitNumber?: string
+}
