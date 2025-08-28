6c66654712c2fdce4e4bb91a2f4502b54a1106b8
deetz99 <dietrich@wolpert.ca>
2025-08-28 12:46:16 -0700

    fix fieldset orientation

--- .changeset/soft-ideas-visit.md
+++ .changeset/soft-ideas-visit.md
@@ -7,5 +7,6 @@ Minor CSS updates
 
 - update footer link divider colour
 - update formfield help/error placeholder spacer
+- fix fieldset orientation
 
 issue: bcgov/entity#30434
--- packages/layers/forms/app/components/Connect/Fieldset.vue
+++ packages/layers/forms/app/components/Connect/Fieldset.vue
@@ -10,33 +10,38 @@ const {
   orientation?: 'vertical' | 'horizontal'
 }>()
 // TODO: figure out why text-error doesnt work on the legend text
+
+const legendId = useId()
 </script>
 
 <template>
-  <fieldset
-    class="flex"
-    :class="[orientation === 'horizontal' ? 'flex-col gap-6 sm:flex-row sm:gap-4' : 'flex-col gap-6']"
-  >
-    <legend
-      class="text-base text-neutral-highlighted font-bold pb-6"
-      :class="[
-        { 'text-red-600': !!error },
-        { 'w-full sm:w-1/4 pb-0': orientation === 'horizontal' },
-      ]"
+  <fieldset :aria-labelledby="legendId">
+    <div
+      class="flex"
+      :class="[orientation === 'horizontal' ? 'flex-col gap-6 sm:flex-row sm:gap-4' : 'flex-col gap-6']"
     >
-      <div class="flex flex-wrap gap-4">
-        <span>{{ label }}</span>
-        <span
-          v-if="!!error && showErrorMsg"
-          class="font-normal"
-        >
-          {{ error.message }}
-        </span>
-      </div>
-    </legend>
+      <legend
+        :id="legendId"
+        class="text-base text-neutral-highlighted font-bold"
+        :class="[
+          { 'text-red-600': !!error },
+          { 'w-full sm:w-1/4 pb-0': orientation === 'horizontal' },
+        ]"
+      >
+        <div class="flex flex-wrap gap-4">
+          <span>{{ label }}</span>
+          <span
+            v-if="!!error && showErrorMsg"
+            class="font-normal"
+          >
+            {{ error.message }}
+          </span>
+        </div>
+      </legend>
 
-    <div class="flex-1">
-      <slot />
+      <div class="flex-1">
+        <slot />
+      </div>
     </div>
   </fieldset>
 </template>
