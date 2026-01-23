0e63472373a028ecb3c42e19be4bc6506cd7bdbe
Cameron Bowler <cameron.bowler@ca.ey.com>
2026-01-23 10:18:39 -0800

    Correct Type for profile mock

--- packages/layers/auth/testMocks/auth/profile/index.ts
+++ packages/layers/auth/testMocks/auth/profile/index.ts
@@ -1,11 +1,9 @@
 import fs from 'fs'
 import { createResolver } from 'nuxt/kit'
 
-import type { ConnectAccount } from '#auth/app/interfaces/connect-account'
-
 const { resolve } = createResolver(import.meta.url)
 
 export const getUserProfileMock = () => {
-  const json: ConnectAccount[] = JSON.parse(fs.readFileSync(resolve('./json/PUBLIC_USER.json'), 'utf8'))
+  const json: ConnectAuthProfile = JSON.parse(fs.readFileSync(resolve('./json/PUBLIC_USER.json'), 'utf8'))
   return json
 }
