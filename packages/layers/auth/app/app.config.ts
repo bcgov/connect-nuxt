cd54d610029fd28f46a1f247ae7a77f72be05b98
deetz99 <dietrich@wolpert.ca>
2026-07-03 08:21:19 -0700

    test

--- packages/layers/auth/app/app.config.ts
+++ packages/layers/auth/app/app.config.ts
@@ -1,4 +1,4 @@
-// import { ConnectIdpHint } from '#imports'
+import { ConnectIdpHint } from './enums/connect-idp-hint'
 import type { AppConfigInput } from 'nuxt/schema'
 
 export default defineAppConfig({
