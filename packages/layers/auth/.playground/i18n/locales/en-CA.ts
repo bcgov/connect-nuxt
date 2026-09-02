333de0842e97569108c3edc33cbdbdb78611da3f
Kial Jinnah <kialj876@gmail.com>
2026-09-02 11:49:10 -0400

    34765 auth - set login page desc from preset
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- packages/layers/auth/.playground/i18n/locales/en-CA.ts
+++ packages/layers/auth/.playground/i18n/locales/en-CA.ts
@@ -5,6 +5,9 @@ export default {
     page: {
       createAccount: {
         description: ['PLAYGROUND ONLY - Create a new account to use your services and manage all of your businesses within BC Registries.']
+      },
+      login: {
+        descriptionCustom: 'PLAYGROUND ONLY - This login description is set by the customDescription preset.'
       }
     }
   }
